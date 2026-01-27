dc7b7a19b1e96d6d8ca86b15356838d47411b808
nhevers <faixzakhan10@gmail.com>
2026-01-27 12:36:00 +0100

    add filters module

---
+++ src/filters/index.ts
@@ -0,0 +1,11 @@
+/**
+ * Filters Module
+ */
+
+export { DateFilter } from './DateFilter.js';
+export type { DatePreset, DateRange, DateFilterOptions } from './DateFilter.js';
+
+export { TypeFilter } from './TypeFilter.js';
+export type { ObservationType } from './TypeFilter.js';
+
+export { ProjectFilter } from './ProjectFilter.js';
