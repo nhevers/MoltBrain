c48bec1f9026bb9fa4644dd418ab7ea35d070891
nhevers <faixzakhan10@gmail.com>
2026-01-27 12:38:26 +0100

    add validators module

---
+++ src/validators/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Validators Module
+ */
+
+export { SettingsValidator } from './SettingsValidator.js';
+export type { ValidationError, ValidationResult, SettingsSchema } from './SettingsValidator.js';
+
+export { ObservationValidator } from './ObservationValidator.js';
+export type { ObservationInput } from './ObservationValidator.js';
