b60ff2ec1dade94dc52f55463cd08e2c40f8df34
nhevers <faixzakhan10@gmail.com>
2026-01-27 12:31:43 +0100

    add themes module

---
+++ src/themes/index.ts
@@ -0,0 +1,11 @@
+/**
+ * Themes Module
+ * 
+ * Exports all theme functionality.
+ */
+
+export { ThemeManager, getThemeManager } from './ThemeManager.js';
+export type { Theme, ThemeColors, ThemeMode, ThemeManagerOptions } from './ThemeManager.js';
+
+export { darkTheme } from './dark.js';
+export { lightTheme } from './light.js';
